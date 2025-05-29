e1a495d94dbb0c32e17fa9f2adfdaecd4e5a6880
Algorhythm <k53952684@gmail.com>
2025-05-29 13:03:28 +0900

    Update mandarin.java
    돌하르방 아이템 추가 구현 및 추가 점수 당 시간 0.5배 회복 구현

--- mandarin.java
+++ mandarin.java
@@ -1,4 +1,4 @@
-package com.example.test4;
+package com.example.mandaringame_test;
 
 import android.content.Context;
 import android.graphics.Color;
@@ -9,6 +9,9 @@ import android.widget.LinearLayout;
 
 public class mandarin extends LinearLayout {
     public int number;
+    public boolean isHanrabong = false;
+    public boolean isDolhareubang = false;
+
     private ImageView imageView;
 
     public mandarin(Context context, int num) {
@@ -22,13 +25,14 @@ public class mandarin extends LinearLayout {
         setLayoutParams(new ViewGroup.LayoutParams(200, 200));
 
         imageView = new ImageView(context);
-        imageView.setImageResource(getImageResIdForNumber(num));
         imageView.setLayoutParams(new LayoutParams(120, 120));
+        imageView.setImageResource(getMandarinResId(num));
         addView(imageView);
     }
 
-    private int getImageResIdForNumber(int number) {
-        switch (number) {
+    private int getMandarinResId(int num) {
+        switch (num) {
+            case 0: return R.drawable.dolhareubang;
             case 1: return R.drawable.mandarin1;
             case 2: return R.drawable.mandarin2;
             case 3: return R.drawable.mandarin3;
@@ -40,5 +44,32 @@ public class mandarin extends LinearLayout {
             case 9: return R.drawable.mandarin9;
             default: return R.drawable.mandarin;
         }
+    }
+
+    private int getHanrabongResId(int num) {
+        switch (num) {
+            case 1: return R.drawable.hanrabong1;
+            case 2: return R.drawable.hanrabong2;
+            case 3: return R.drawable.hanrabong3;
+            case 4: return R.drawable.hanrabong4;
+            case 5: return R.drawable.hanrabong5;
+            case 6: return R.drawable.hanrabong6;
+            case 7: return R.drawable.hanrabong7;
+            case 8: return R.drawable.hanrabong8;
+            case 9: return R.drawable.hanrabong9;
+            default: return R.drawable.hanrabong;
+        }
+    }
+
+    public void markAsHanrabong() {
+        isHanrabong = true;
+        setBackgroundColor(Color.parseColor("#89B45B"));
+        imageView.setImageResource(getHanrabongResId(number));
+    }
+
+    public void markAsDolhareubang() {
+        isDolhareubang = true;
+        setBackgroundColor(Color.GRAY);
+        imageView.setImageResource(R.drawable.dolhareubang);
     }
 }
